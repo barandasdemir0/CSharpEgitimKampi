8d74040d07e0adf106f0df106cd8b27044044cbf
Your Name <barandasdemir.bd@gmail.com>
2025-06-30 19:35:35 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region yazdırma komutları  (print commands)
+            //Console.Write("hello world");   --> write yazmak
+            //Console.WriteLine("selam"); --> writeline yazma satırı
+            #endregion
+
+            #region string değişkenler (variable)
+
+            //string name;
+            //name = "baran";
+            //Console.WriteLine(name);
+
+            string customerName;
+            string customerSurname;
+            string customerPhone;
+            string customerEmail, district, city;
+
+
+            //camelcase customerName   district = semt
+
+            customerName = "baran";
+            customerSurname = "daşdemir";
+            customerPhone = "05554442233";
+            customerEmail = "denemeler@example.com";
+            district = "keçiören";
+            city = "ankara";
+
+
+            Console.WriteLine(customerName+" "+customerSurname + " " + customerPhone + " " + customerEmail + " " + district + " " + city);
+
+
+            #endregion
+
+            #region int değişkenler  integer = tam sayı  price = ücret
+            int sayi= 25;
+            Console.WriteLine(sayi);
+
+            #endregion
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ 02_Variables/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region double değişkenler
+            double number = 0.34;
+            Console.WriteLine(number);
+            #endregion
+
+            #region char değişkenler
+            char harf = 'a';
+            Console.WriteLine(harf);
+
+            #endregion
+
+            #region klavyeden veri girişi
+            Console.Write("isminiz nedir = ");
+            string putName = Console.ReadLine();
+            Console.Write("soyisminiz nedir = ");
+            string putSurname = Console.ReadLine();
+
+            Console.WriteLine("İsim soyisim : " + putName + " " + putSurname);
+
+            #endregion
+
+            #region klavyaden tam sayı girişleri ve dönüşümler
+            int shoesPrice;
+            shoesPrice = 1000;
+            int shoesCount;
+            Console.Write("kaç ayakkabı aldın : ");
+            shoesCount = int.Parse(Console.ReadLine());
+            int totalPrice = shoesCount * shoesPrice;
+            Console.Write("toplam ödeyeceğin ücret : " + totalPrice + " tl");
+
+
+            #endregion
+
+            #region klavyeden ondalıklı sayı girme
+            double exam1, exam2, result;
+            Console.Write("exam1  = ");
+            exam1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("exam2  = ");
+            exam2 = Convert.ToDouble(Console.ReadLine());
+      
+
+           
+                result = (exam1 % 40) + (exam2 % 60);
+                Console.Write(result);
+            
+          
+            
+
+            Console.Read();
+        }
+
+
+
+
+        #endregion
+    }
+
+}
+            
