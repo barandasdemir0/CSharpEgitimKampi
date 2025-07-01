8418e7ddc50bedb9cc40c23c27fb13b05b8225c6
Your Name <barandasdemir.bd@gmail.com>
2025-07-01 10:18:48 +0300

    class_4_5

---
+++ 03_MakingDecision/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_MakingDecision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region if 
+            //Console.Write("Lütfen Şifrenizi giriniz");
+            //string password = Console.ReadLine();
+            //if (password == "abcd")
+            //{
+            //    Console.WriteLine("şifreniz doğru");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("şifreniz yanlış");
+            //}
+            //Console.Read();
+
+            //string country, capital;
+            //Console.Write("Başkenti giriniz : ");
+            //capital = Console.ReadLine();
+            //Console.Write("Ülkeyi giriniz : ");
+            //country = Console.ReadLine();
+
+            //if (country=="türkiye"&&capital=="ankara")
+            //{;
+            //    Console.Write("veriler doğru");
+            //}
+            //else
+            //{
+            //    Console.Write("veriler yanlış");   
+            //}
+
+            //double exam1,exam2,result;
+
+            //Console.Write("1.sınav sonucu : ");
+            //exam1 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("2.sınav sonucu : ");
+            //exam2 = Convert.ToInt32(Console.ReadLine());
+            //result = (exam1 * 0.4) + (exam2 * 0.6);
+            //if (result>=50)
+            //{
+            //    Console.Write("Büte kalmadın");
+            //}
+            //else
+            //{
+            //    Console.Write("büt dayanır kapıya");
+            //}
+
+            //Console.Write("Kullanıcı adı giriniz : ");
+            //string username = Console.ReadLine();
+            //if (username != "admin")
+            //{
+            //    Console.Write("Kullanıcı giriş yapamaz");
+            //}
+            //else
+            //{
+            //    Console.Write("kullanıcı giriş yapar");
+            //}
+
+
+
+
+
+
+            #endregion
+
+            #region mod işlemleri
+            //int number = 26;
+            //int result = number % 5;
+            //Console.WriteLine(result);
+
+            //Console.Write("Sayıyı gir bea : ");
+            //int number = int.Parse(Console.ReadLine());
+            //Console.Write("Kaçla modunu alalım : ");
+            //int mod = int.Parse(Console.ReadLine());
+            //int result = number % mod;
+            //Console.Write("Sonuç : "+result);
+
+            #endregion
+
+            #region switch case
+
+            //Console.Write("Ay girişi yapınız : ");
+            //int mountNumber = int.Parse(Console.ReadLine());
+
+            //switch(mountNumber)
+            //{
+            //    case 1: Console.Write("Ocak"); break;
+            //    case 2: Console.Write("Şubat"); break;
+            //    case 3: Console.Write("Mart"); break;
+            //    case 4: Console.Write("nisan"); break;
+            //    case 5: Console.Write("mayıs"); break;
+            //    case 6: Console.Write("haziran"); break;
+            //    case 7: Console.Write("temmuz"); break;
+            //    case 8: Console.Write("ağustos"); break;
+            //    case 9: Console.Write("eylül"); break;
+            //    case 10: Console.Write("ekim"); break;
+            //    case 11: Console.Write("kasım"); break;
+            //    case 12: Console.Write("aralıl"); break;
+            //    default: Console.Write("Ay bulunamadı"); break;
+            //}
+
+            #endregion
+
+            #region hesap makinesi
+
+            Console.Write("1.sayıyı giriniz : ");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.Write("2.sayıyı giriniz : ");
+            int number2 = int.Parse(Console.ReadLine());
+            int result;
+            Console.Write("Yapmak istediğiniz işlem : ");
+            char symbol = char.Parse( Console.ReadLine());
+
+            switch(symbol)
+            {
+                case '+': 
+                    result= number1 + number2;
+                    Console.Write("Sonucumuz : " + result);
+                    break;
+                case '-':
+                    result = number1 - number2;
+                    Console.Write("Sonucumuz : " + result);
+                    break;
+                case '*':
+                    result = number1 * number2;
+                    Console.Write("Sonucumuz : " + result);
+                    break;
+                case '/':
+                    result = number1 / number2;
+                    Console.Write("Sonucumuz : " + result);
+                    break;
+            }
+
+            #endregion  
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ 04_Loops/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region for döngüsü
+            //for (int i = 0; i <= 5; i++)
+            //{
+            //    Console.WriteLine("Atatürk");
+            //}
+            //for (int i = 1; i < 20; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //for (int i = 0; i < 50; i+=3)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.Write("Ne yazmak istiyorsun : ");
+            //string veri = Console.ReadLine();
+            //Console.Write("ne kadar yazdırmak istiyorsun : ");
+            //int number = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= number; i++)
+            //{
+            //    Console.WriteLine(i+".değer "+veri);
+            //}
+
+            #endregion
+
+            #region for döngüsü ile kara yapıları
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //int totalvalue=0;
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    totalvalue += i;
+
+
+            //}
+            //Console.WriteLine(totalvalue);
+
+            //int totalValue = 0;
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    if (i%2==0)
+            //    {
+            //        totalValue = totalValue + i;
+            //        Console.WriteLine(i);
+            //    }
+
+
+            //}
+            //Console.WriteLine(totalValue);
+
+            //int bacterium = 1;
+            //for (int i = 0; i <= 24; i++)
+            //{
+            //    bacterium = bacterium * 2;
+            //    Console.WriteLine(i+".saat sonunda oluşan bakteri sayısı : "+bacterium);
+            //}
+
+
+            #endregion
+
+            #region while döngüsü
+
+            //int yazdir = 1;
+            //while (yazdir<=10)
+            //{
+            //    Console.WriteLine("merhaba while");
+            //    yazdir++;
+
+            //}
+
+
+            //int i = 1;
+            //while (i <= 10) 
+            //{
+            //    if (i%3==0)
+            //    {
+            //        Console.WriteLine(i); 
+
+            //    }
+            //    i++;
+            //}
+
+            //int i = 1;
+            //int sum = 0;
+
+            //while (i <= 10)
+            //{
+            //    sum += i;
+
+            //    i++;
+
+            //}
+            //Console.WriteLine(sum);
+
+
+
+            #endregion
+
+            #region örnek sınav sorusu
+
+            //kullanıcıdan veriyi al sonra sayıyı basamaklarına ayır ve sonar her ayırdığın basamakları topla
+
+            //Console.Write("Sayınızı giriniz : ");
+            //int sayi = int.Parse(Console.ReadLine());
+
+            //int ones, tens, hundreds,sum;
+            //ones = sayi % 10;
+            //tens = (sayi % 100)/10;
+            //hundreds = (sayi / 100);
+            //sum = ones+tens+hundreds;
+            //Console.WriteLine(sum);
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
