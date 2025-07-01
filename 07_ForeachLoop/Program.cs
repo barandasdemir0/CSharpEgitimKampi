3e7910d82c390c68238e9bd42c56b4ee70c6dba6
Your Name <barandasdemir.bd@gmail.com>
2025-07-01 16:49:38 +0300

    class_7_foreach

---
+++ 07_ForeachLoop/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ForeachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region foreach
+
+
+            //string[] cities = { "Ankara", "Roma", "Budapeşte", "Ötüken", "Atina", "Londra"};
+            //foreach (string city in cities)
+            //{
+            //    Console.WriteLine(city);
+            //}
+
+
+            //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            //foreach (int i in numbers)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            //foreach (int i in numbers)
+            //{
+            //    if (i%2==0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+
+            //}
+
+
+
+            //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            //int totalnumber = 0;
+            //foreach (int i in numbers)
+            //{
+            //    totalnumber += i;
+            //}
+            //Console.WriteLine(totalnumber);
+
+            //List<int> numbers = new List<int>() //içinde integer olan bir liste oluşturduk
+            //{
+            //   1, 2, 3, 4, 5, 6, 7
+            //};
+            //foreach (int i in numbers)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //string isim = "merhaba";
+            //foreach (char a in isim)
+            //{
+            //    Console.WriteLine(a);
+            //}
+
+            #endregion
+
+
+
+
+
+
+
+
+
+            Console.Read();
+        }
+    }
+}
