294ffa29a367600f10118fcb4319b9a134774fa9
Your Name <barandasdemir.bd@gmail.com>
2025-07-01 19:04:28 +0300

    class_09_database

---
+++ 09_DatabaseProject/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_DatabaseProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Ado.net
+
+            string tableNumber;
+            Console.WriteLine("***** ürün kategori sistemi *****");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış Yap");
+
+            Console.Write("Lütfen Getirmek İstediğiniz tablonun numarasını girin : ");
+            tableNumber = Console.ReadLine();
+
+
+            SqlConnection connection = new SqlConnection("Data Source = BARAN;initial Catalog=EgitimKampiDb;integrated security=true");
+            connection.Open();
+            SqlCommand command = new SqlCommand("select * from TblCategory",connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            connection.Close();
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                foreach (var item in row.ItemArray)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+
+
+            Console.Read();
+        }
+    }
+}
